00968f049f90a69cc4284d98561c28212e9f464a
Shamsad Alam Meraj <shamsadalammeraj9@outlook.com>
2025-01-13 22:36:15 +0600

    create & update route added

--- src/app/modules/products/products.routes.ts
+++ src/app/modules/products/products.routes.ts
@@ -3,6 +3,13 @@ import { ProductController } from './products.controller';
 
 const router = express.Router();
 
+// Get the product list as a normal user
 router.get('/', ProductController.getProducts);
+// Create Product As an Admin
+router.post('/admin/create', ProductController.createProduct);
+// Update Product as an Admin
+router.put('/admin/update', ProductController.updateProduct);
+
+export default router;
 
 export const ProductRoutes = router;
