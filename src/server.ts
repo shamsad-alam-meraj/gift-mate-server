1401a14c96393989b5003a91124a057a339733be
Shamsad Alam Meraj <shamsadalammeraj9@outlook.com>
2025-01-13 15:38:56 +0600

    db connected

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
 
 async function main() {
   try {
-    // await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
       console.log(`Gift Mate app listening on port ${config.port}`);
